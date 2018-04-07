dde0fedb804144a5a94132ae9800333d1e384f2e
Mark Wallsgrove <mark.wallsgrove+git@gmail.com>
2018-04-07 14:23:18 +0100

    remove

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	logger := log.New(os.Stdout, "bot: ", log.Lshortfile|log.LstdFlags)
-	bot := bot.MakeBot(logger, "xoxb-329560334997-dhVE3F5IMMFP2u8Kz9sFOubu")
+	bot := bot.MakeBot(logger, "")
 	bot.Register(workers.MakeTestWorker(logger))
 	bot.Register(workers.MakeOutputWorker(logger))
 	bot.Listen()
